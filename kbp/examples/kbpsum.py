29a05df26001d62bc825b39ba165dcebd29a6154
ange.albertini <ange.albertini@2aaee85a-a46a-11dd-b47b-0378ca0a891c>
2009-02-26 08:09:56 +0000

    now using commong family code, and started standardizing display and structure

--- kbp/examples/kbpsum.py
+++ kbp/examples/kbpsum.py
@@ -2,7 +2,7 @@ from getopt import GetoptError, getopt
 from sys import argv, exit
 
 __revision__ = "$LastChangedRevision$"
-__version__ = '0.1-%d' % int( __revision__.split()[1])
+__version__ = '0.1r%d' % int( __revision__.split()[1])
 
 
 try:
@@ -12,7 +12,7 @@ except:
     pass
 
 import kbp.checksum.crc as crc
-import kbp.crypt.md2    as md4
+import kbp.crypt.md2    as md2
 import kbp.crypt.md4    as md4
 import kbp.crypt.has    as has
 import kbp.crypt.sha    as sha
@@ -22,43 +22,65 @@ import kbp.crypt.tiger  as tiger
 
 digesttest = lambda m, s:m.compute(s).hexdigest()
 
-families = [
-    "crc", "has", "md", "ripemd", "sha", "tiger", 
-    #"adler", "flechter", "gost", "haval", "lm", "panama", "whirlpool", "snefru",
-    ]
-algorithms = {
-    "crc32_ieee":crc.crc32_ieee_hexhash,
-    "has-160":lambda x:has.Has160().compute(x).hexdigest(),
-    "md2":lambda x:md2.Md2().compute(x).hexdigest(),
-    "md4":lambda x:md4.Md4().compute(x).hexdigest(),
-    "md5":lambda x:md4.Md5().compute(x).hexdigest(),
-    "ripemd-128":lambda x:ripemd.Ripemd128().compute(x).hexdigest(),
-    "ripemd-160":lambda x:ripemd.Ripemd160().compute(x).hexdigest(),
-    "ripemd-256":lambda x:ripemd.Ripemd256().compute(x).hexdigest(),
-    "ripemd-320":lambda x:ripemd.Ripemd320().compute(x).hexdigest(),
-    "sha-0"     :lambda x:sha.Sha0().compute(x).hexdigest(),
-    "sha-1"     :lambda x:sha.Sha1().compute(x).hexdigest(),
-    "sha-224"   :lambda x:sha2.Sha224().compute(x).hexdigest(),
-    "sha-256"   :lambda x:sha2.Sha256().compute(x).hexdigest(),
-    "sha-384"   :lambda x:sha2.Sha384().compute(x).hexdigest(),
-    "sha-512"   :lambda x:sha2.Sha512().compute(x).hexdigest(),
-    "tiger"     :lambda x:tiger.Tiger().compute(x).hexdigest(),
-    "tiger2"    :lambda x:tiger.Tiger2().compute(x).hexdigest(),
-    "tiger128"  :lambda x:tiger.Tiger128().compute(x).hexdigest(),
-    "tiger160"  :lambda x:tiger.Tiger160().compute(x).hexdigest(),
-    #"adler32":_,
-    #"flechter16":_,
-    #"flechter32":_,
-    #"gost":_,
-    #"haval":_,
-    #"panama":_,
-    #"lm":_,
-    #"snefru":_,
-    #"whirlpool":_,
+families = {
+    "CRC":{
+        "crc32_ieee":crc.crc32_ieee_hexhash,
+        },
+    "HAS":{
+        "HAS-160":lambda x:has.Has160().compute(x).hexdigest(),
+        },
+    "MD":{
+        "MD2":lambda x:md2.Md2().compute(x).hexdigest(),
+        "MD4":lambda x:md4.Md4().compute(x).hexdigest(),
+        "MD5":lambda x:md4.Md5().compute(x).hexdigest(),
+        },
+    "RIPEMD":{
+        "RIPEMD-128":lambda x:ripemd.Ripemd128().compute(x).hexdigest(),
+        "RIPEMD-160":lambda x:ripemd.Ripemd160().compute(x).hexdigest(),
+        "RIPEMD-256":lambda x:ripemd.Ripemd256().compute(x).hexdigest(),
+        "RIPEMD-320":lambda x:ripemd.Ripemd320().compute(x).hexdigest(),
+        },
+    "SHA":{
+        "SHA-0"     :lambda x:sha.Sha0().compute(x).hexdigest(),
+        "SHA-1"     :lambda x:sha.Sha1().compute(x).hexdigest(),
+        },
+    "SHA-2":{
+        "SHA-224"   :lambda x:sha2.Sha224().compute(x).hexdigest(),
+        "SHA-256"   :lambda x:sha2.Sha256().compute(x).hexdigest(),
+        "SHA-384"   :lambda x:sha2.Sha384().compute(x).hexdigest(),
+        "SHA-512"   :lambda x:sha2.Sha512().compute(x).hexdigest(),
+        },
+    "tiger":{
+        "tiger"     :lambda x:tiger.Tiger().compute(x).hexdigest(),
+        "tiger2"    :lambda x:tiger.Tiger2().compute(x).hexdigest(),
+        "tiger128"  :lambda x:tiger.Tiger128().compute(x).hexdigest(),
+        "tiger160"  :lambda x:tiger.Tiger160().compute(x).hexdigest(),
+        }
     }
-Help = "Kabopan checksum calculator\n"
+
+algorithms = list()
+for f in families.itervalues():
+    algorithms.extend(list(f.iteritems()))
+algorithms = dict(algorithms)
+
+#"adler32":_,
+#"flechter16":_,
+#"flechter32":_,
+#"gost":_,
+#"haval":_,
+#"panama":_,
+#"lm":_,
+#"snefru":_,
+#"whirlpool":_,
+
+Help = "Kabopan %s v%s\n" % ("checksum calculator", __version__)
 Help +=  "Parameters:  [-a algorithm] <[-f <input file>]|[<text>]>\n"
-Help += "Algorithms:\n\t"+ "\n\t".join([", ".join([a for a in sorted(algorithms) if a.startswith(f)]) for f in families])
+Help += "Algorithms:\n"
+for f, algs in families.iteritems():
+    Help += "\t%s\n" % f
+    for alg in algs:
+        Help += "\t\t%s\n" % alg
+Help += "\n"
 
 if len(argv) == 1:
     print(Help)
